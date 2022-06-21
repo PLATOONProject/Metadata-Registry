5177b33d853b824e64f114ad8df1f18c6e8273ad
TasneemRashid <rashid.tasneem@gmail.com>
2022-06-21 16:24:41 +0200

    updated tests for AppHandler

--- docker/buildImages.sh
+++ docker/buildImages.sh
@@ -7,7 +7,7 @@ dos2unix broker-core/*
 mvn -f ../ clean package
 rm broker-core/broker-core-*.jar
 cp ../broker-core/target/broker-core-*.jar broker-core/
-docker build broker-core/ -t registry.gitlab.cc-asp.fraunhofer.de/eis-ids/broker-open/core
+docker build broker-core/ -t registry.gitlab.cc-asp.fraunhofer.de/metadata-registry/core:1.0.2
 
 #cleanup
 rm -rf ../index-common/target
@@ -15,7 +15,7 @@ rm -rf ../broker-common/target
 
 
 # fuseki
-docker build fuseki/ -t registry.gitlab.cc-asp.fraunhofer.de/eis-ids/broker-open/fuseki
+#docker build fuseki/ -t registry.gitlab.cc-asp.fraunhofer.de/eis-ids/broker-open/fuseki
 
 # reverseproxy
-docker build reverseproxy/ -t registry.gitlab.cc-asp.fraunhofer.de/eis-ids/broker-open/reverseproxy
+#docker build reverseproxy/ -t registry.gitlab.cc-asp.fraunhofer.de/eis-ids/broker-open/reverseproxy
